8fa4a1e7d38fa43377444ca23d343fa7445d92ae
Yung Chun Lu <yungchunlu@Yungs-MacBook-Pro.local>
2019-09-22 16:07:19 +0700

    initialization

---
+++ producers/models/.ipynb_checkpoints/turnstile-checkpoint.py
@@ -0,0 +1,90 @@
+"""Creates a turnstile data producer"""
+import logging
+from pathlib import Path
+
+from confluent_kafka import avro
+
+from models.producer import Producer
+from models.turnstile_hardware import TurnstileHardware
+
+
+logger = logging.getLogger(__name__)
+
+
+class Turnstile(Producer):
+    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+
+    #
+    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
+    #
+    value_schema = avro.load(
+       f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
+
+    def __init__(self, station):
+        """Create the Turnstile"""
+        station_name = (
+            station.name.lower()
+            .replace("/", "_and_")
+            .replace(" ", "_")
+            .replace("-", "_")
+            .replace("'", "")
+        )
+
+        #
+        #
+        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
+        # replicas
+        #
+        #
+        topic_name = f"org.chicago.cta.station.arrivals.turnstile_data.v1"
+        super().__init__(
+            topic_name,
+            key_schema=Turnstile.key_schema,
+            value_schema=Turnstile.value_schema,
+            num_partitions=1,
+            num_replicas=1
+        )
+        self.station = station
+        self.turnstile_hardware = TurnstileHardware(station)
+
+    def __str__(self):
+        return "Station | {:^5} | {:<30} | Line: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
+            self.station.station_id,
+            self.station.name,
+            self.station.color,
+            self.dir_a.name if self.dir_a is not None else "---",
+            self.b_train.train_id if self.b_train is not None else "---",
+            self.dir_b.name if self.dir_b is not None else "---",
+        )
+    
+    def run(self, timestamp, time_step):
+        """Simulates riders entering through the turnstile."""
+        num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
+        #
+        #
+        # TODO: Complete this function by emitting a message to the turnstile topic for the number
+        # of entries that were calculated
+        #
+        #
+        self.producer.produce(
+           topic=self.topic_name,
+           key={"timestamp": self.time_millis()},
+           value={
+               "station_id": self.station.station_id,
+               "station_name": self.station.name,
+               "line": self.station.color.name,
+               "num_entries": num_entries
+           }
+        )
+        
+        info = "Station | {:^5} | {:<30} | Line: | {:^5} | Num Entries: | {:^5}".format(
+            self.station.station_id,
+            self.station.name,
+            self.station.color,
+            num_entries
+        )
+        logger.debug(
+            "sent turnstile data to kafka\n\t%s",
+            info
+        )
